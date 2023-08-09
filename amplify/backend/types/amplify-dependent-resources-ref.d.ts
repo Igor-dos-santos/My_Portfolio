20d596dd9941c20d7c6ae6b47693450e2b52394a
Igor Dos Santos <a2sigor@gmail.com>
2023-08-09 11:44:11 +0100

    Home page and images updated

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,11 @@
+export type AmplifyDependentResourcesAttributes = {
+  "function": {
+    "portfolioigore48064cd": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
